dae18fbde03b9ede5d438c4c1ca1ed7dc7d7c290
antonioalexandre1984 <antonioalexandre1984@gmail.com>
2026-02-17 10:21:40 -0300

    adicionado dominio da vercel ao settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -145,6 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://appfood-seven.vercel.app/"
 ]
 
 STATIC_URL = '/static/'
